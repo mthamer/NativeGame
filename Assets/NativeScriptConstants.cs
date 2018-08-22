02a006793e65ccdb31231f8ee96fd0848a749e9d
BHG\mthamer <mthamer@bighugegames.com>
2018-08-21 23:00:23 -0400

    sample bindings

---
+++ Assets/NativeScriptConstants.cs
@@ -0,0 +1,17 @@
+/// <summary>
+/// Constants used by the C++ scripting system. Redefine these with values
+/// specific to your project.
+/// </summary>
+/// <author>
+/// Jackson Dunstan, 2017, http://JacksonDunstan.com
+/// </author>
+/// <license>
+/// MIT
+/// </license>
+public static class NativeScriptConstants
+{
+	/// <summary>
+	/// Path within the Unity project to the exposed types JSON file
+	/// </summary>
+	public const string JSON_CONFIG_PATH = "NativeScriptTypes.json";
+}
